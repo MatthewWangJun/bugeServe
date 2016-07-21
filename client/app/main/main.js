b84d3147c9c91809920e388542d96af3dc9153a5
王俊 <wangjun@dinglicom.com>
2016-07-21 20:19:55 +0800

    index fix

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -3,12 +3,16 @@
 angular.module('bugeSeverApp')
   .config(function($stateProvider) {
     $stateProvider.state('main', {
-      url: '/',
-      template: '<main></main>'
+      url: '/main',
+        views: {
+            "context@": {
+                templateUrl:  'app/main/main.html',
+                controller:'mainCtrl'
+            }
 
-    })
+    }})
     .state('index', {
-          url: '/index',
+          url: '/',
           views: {
             "context@": {
               templateUrl: 'app/main/index.html',
